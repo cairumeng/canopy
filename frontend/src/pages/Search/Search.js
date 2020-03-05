8ef5810ac9242429b30f834985c9e579826cbf8f
cairumeng <cairumeng@gmail.com>
2020-03-05 17:13:00 +0100

    Search

---
+++ frontend/src/pages/Search/Search.js
@@ -0,0 +1,124 @@
+import React from 'react'
+import queryString from 'query-string'
+import { connect } from 'react-redux'
+import ProductList from '../../components/ProductList/ProductList'
+import Loading from '../../components/Loading/Loading'
+import { getProducts } from '../../redux/actions/product'
+import './Search.scss'
+
+class Search extends React.Component {
+  scrollFunc = null
+  searchText = ''
+
+  state = {
+    isAutoLoading: false,
+  }
+
+  handleScroll = () => {
+    const windowHeight =
+      'innerHeight' in window
+        ? window.innerHeight
+        : document.documentElement.offsetHeight
+    const html = document.documentElement
+    const docHeight = Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    )
+    const windowBottom = windowHeight + window.pageYOffset
+
+    const { lastPage, pageIndex, isLoading } = this.props.productsState
+
+    if (
+      windowBottom + 200 >= docHeight &&
+      !isLoading &&
+      pageIndex <= lastPage
+    ) {
+      this.setState({
+        isLoading: true,
+      })
+      this.props.getProducts(pageIndex + 1, this.searchText)
+    }
+  }
+
+  onLoadMoreHandler = () => {
+    this.setState({
+      isAutoLoading: true,
+    })
+    this.scrollFunc = window.addEventListener('scroll', this.handleScroll, true)
+
+    this.props.getProducts(
+      this.props.productsState.pageIndex + 1,
+      this.searchText
+    )
+  }
+
+  componentDidMount() {
+    console.log(queryString.parse(this.props.location.search))
+    this.searchText = queryString.parse(this.props.location.search).searchText
+
+    console.log(this.searchText)
+
+    this.setState({
+      isLoading: true,
+    })
+    this.props.getProducts(1, this.searchText).then(response => {
+      this.setState({
+        categoryName: response.categoryName,
+      })
+    })
+  }
+
+  componentWillUnmount() {
+    // all addEventListenr & setInterval operation should be free
+    window.removeEventListener('scroll', this.scrollFunc)
+  }
+
+  render() {
+    const {
+      products,
+      lastPage,
+      pageIndex,
+      isLoading,
+    } = this.props.productsState
+
+    return (
+      <>
+        <div className="pageHeader">
+          <div className="pageHeader-content">Search Result</div>
+        </div>
+
+        <div className="container mt-5">
+          <ProductList products={products} />
+        </div>
+
+        {isLoading && <Loading />}
+
+        {!this.state.isAutoLoading && !isLoading && (
+          <div className="LoadMoreButton" onClick={this.onLoadMoreHandler}>
+            Load more
+          </div>
+        )}
+
+        {!isLoading && pageIndex >= lastPage && <div>No more products</div>}
+      </>
+    )
+  }
+}
+
+const mapStateToProps = store => {
+  return {
+    productsState: store.getProducts,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getProducts: (pageIndex, searchText) =>
+      dispatch(getProducts(pageIndex, searchText)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search)
