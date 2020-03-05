f02c2aecd5e1d6d1d40ed87718a70ecbc6b00575
cairumeng <cairumeng@gmail.com>
2020-03-05 11:02:00 +0100

    Profile

---
+++ frontend/src/pages/Profile/Profile.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { getProfile } from '../../redux/actions/user'
+import './Profile.css'
+
+const Profile = props => {
+  const { name, avatar, nickname, description } = props.profileState.user
+
+  useEffect(() => {
+    props.getProfile()
+  }, [])
+
+  return (
+    <div className=" container-fluide user-header">
+      <img src={avatar} className="user-avatar" alt="user-avatar" />
+      <br />
+      <div className="user-name">{name}</div>
+      <div className="user-nickname">{nickname}</div>
+      <div className="mt-3">{description}</div>
+    </div>
+  )
+}
+
+const mapStateToProps = store => {
+  return {
+    profileState: store.getProfile,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getProfile: () => dispatch(getProfile()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile)
